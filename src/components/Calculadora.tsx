fe7b6d7c5d99edf5c05f91241965fdf9628a16a9
LucasThierry39 <113150145+LucasThierry39@users.noreply.github.com>
2023-09-11 19:12:31 -0300

    1 file modified

--- src/components/Calculadora.tsx
+++ src/components/Calculadora.tsx
@@ -1,14 +1,24 @@
+/*
+useState: É usado para gerenciar informações mutáveis dos componentes
+for(let i = 0; i < l1; i++)
+*/
+
+import { useState } from 'react';
+
 export function Calculadora() {
+  const [txtValor1, setTxtValor1] = useState('');
+  const [txtValor2, setTxtValor2] = useState('');
+  const [resultado, setResultado] = useState(0);
   return (
     <div>
-      <label>Valor 1:</label>
+      <label>Valor 1:</label> <br />
       <input type="text" name="valor1" />
-      <br />
-      <label>Valor 2:</label>
+      <br /> <br />
+      <label>Valor 2:</label> <br />
       <input type="text" name="valor2" />
       <br />
-      <label>Resultado</label>
-      <input type="text" name="resultado" />
+      <label>Resultado</label> <br />
+      <input type="text" name="resultado" readOnly />
       <br />
       <button>Calcular</button>
     </div>
