301bfab99b933854945e339e4b8ce3b13d550efb
LucasThierry39 <113150145+LucasThierry39@users.noreply.github.com>
2023-09-11 19:27:06 -0300

    1 file modified

--- src/components/Calculadora.tsx
+++ src/components/Calculadora.tsx
@@ -9,18 +9,38 @@ export function Calculadora() {
   const [txtValor1, setTxtValor1] = useState('');
   const [txtValor2, setTxtValor2] = useState('');
   const [resultado, setResultado] = useState(0);
+
+  const calcular = () => {
+    if (!txtValor1 || !txtValor2) {
+      alert('Prencha os valores antes de executar');
+    }
+    const valor1 = parseFloat(txtValor1);
+    const valor2 = parseFloat(txtValor2);
+    const total = valor1 + valor2;
+    setResultado(parseFloat(total.toFixed(2)));
+  };
   return (
     <div>
       <label>Valor 1:</label> <br />
-      <input type="text" name="valor1" />
+      <input
+        type="text"
+        name="valor1"
+        onChange={(e) => setTxtValor1(e.target.value)}
+      />
       <br /> <br />
       <label>Valor 2:</label> <br />
-      <input type="text" name="valor2" />
+      <input
+        type="text"
+        name="valor2"
+        onChange={(e) => setTxtValor2(e.target.value)}
+      />
       <br />
       <label>Resultado</label> <br />
-      <input type="text" name="resultado" readOnly />
+      <input type="text"
+       name="resultado" 
+       readOnly value={resultado}/>
       <br />
-      <button>Calcular</button>
+      <button onClick={calcular}>Calcular</button>
     </div>
   );
 }
