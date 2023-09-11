f8e8ea6bb373d413de450fc1a5f7fcda2c4b37c0
LucasThierry39 <113150145+LucasThierry39@users.noreply.github.com>
2023-09-11 18:47:44 -0300

    1 file modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-export function App(){
-  return(
+import { Calculadora } from "./components/Calculadora";
+
+/*
+Hooks são funçoes que automatização algums ações no React
+*/
+export function App() {
+  return (
     <div>
       <h1>Exemplo de uso dos Hooks</h1>
+      <Calculadora />
     </div>
-  )
+  );
 }
---
+++ src/components/Calculadora.tsx
@@ -0,0 +1,16 @@
+export function Calculadora() {
+  return (
+    <div>
+      <label>Valor 1:</label>
+      <input type="text" name="valor1" />
+      <br />
+      <label>Valor 2:</label>
+      <input type="text" name="valor2" />
+      <br />
+      <label>Resultado</label>
+      <input type="text" name="resultado" />
+      <br />
+      <button>Calcular</button>
+    </div>
+  );
+}
